48b3a896cd18de8adeecf96e4bbf2f840d26d2df
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2023-01-25 21:51:58 +0200

    Added bmi calculation and display.

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,13 +43,16 @@ class MainActivity : ComponentActivity() {
 fun Bmi() {
     var heightInput: String by remember { mutableStateOf("") }
     var weightInput: String by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val bmi = if (weight > 0 && height > 0.0 ) weight/(height * height) else 0.0
 
     Column(
         modifier = Modifier.padding(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(
-            text = "Body mass index",
+            text = stringResource(R.string.body_mass_index),
             fontSize = 24.sp,
             color = MaterialTheme.colors.primary,
             textAlign = TextAlign.Center,
@@ -59,7 +63,7 @@ fun Bmi() {
         OutlinedTextField(
             value = heightInput,
             onValueChange = {heightInput = it.replace(",",".")},
-            label = { Text("Height")},
+            label = { Text(stringResource(R.string.height))},
             singleLine = true,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier.fillMaxWidth()
@@ -67,10 +71,11 @@ fun Bmi() {
         OutlinedTextField(
             value = weightInput,
             onValueChange = {weightInput = it.replace(",",".")},
-            label = {Text("Weight")},
+            label = {Text(stringResource(R.string.weight))},
             singleLine = true,
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             modifier = Modifier.fillMaxWidth()
         )
+        Text(text = stringResource(R.string.result, String.format("%.2f", bmi).replace(",",".")))
     }
 }
